391f2698c75a57664781b04885bc99e76da37279
Savitha <savithapremkumar@gmail.com>
2023-04-06 10:03:31 +1200

    App styling Added the form with custom hook for validation/error logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import Navigation from './components/Navigation';
+import Menu from './components/Menu';
+import Form from './components/Form';
+import Message from './components/Message';
+import { Constants } from './constants/default';
+
+
 
 function App() {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div className="App">
+      <Navigation handleClick={handleClick} showMenu={showMenu} aria-haspopup />
+      <Message message={showMenu ? Constants.MENU_MESSAGE : Constants.DEFAULT_MESSAGE}></Message>
+      {showMenu ? <Menu /> : <Form />}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { useForm } from './hooks/useForm';
 
 
-const Form = () => {
-
-    return (<form></form>)
+export interface CardNumberDetails {
+    cardNumber: number;
+    cvv: number;
+    expiry: string;
 }
 
+
+const Form: FC = () => {
+    const { handleSubmit, handleChange, data: cardDetails, errors } = useForm<CardNumberDetails>({
+        validations: {
+            cardNumber: {
+                custom: {
+                    isValid: (value) => value.length >= 10,
+                    message:
+                        "Card number has to be a minimum of 10 digits",
+                },
+            },
+            cvv: {
+                custom: {
+                    isValid: (value) => value.length === 3,
+                    message: 'CVV has to be a minimum of 3 numbers',
+                },
+            },
+            expiry: {
+                custom: {
+                    isValid: (value) => new Date(value).toISOString().split('T') > new Date().toISOString().split('T'),
+                    message: 'Expiry has to be in the future'
+                }
+            },
+        },
+        onSubmit: () => {
+            alert('submitted')
+            console.log(cardDetails);
+        },
+    });
+
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            <div className="row">
+                <input
+                    aria-label="Credit card number"
+                    placeholder="Credit card number*"
+                    value={cardDetails.cardNumber || ''}
+                    onChange={handleChange<number>('cardNumber', (value) => parseInt(value, 10))}
+                    required
+                />
+                {errors.cardNumber && <p className="error">{errors.cardNumber}</p>}
+            </div>
+            <div className="row">
+                <input
+                    aria-label="CVV"
+                    placeholder="CVV"
+                    type="number"
+                    value={cardDetails.cvv || ''}
+                    onChange={handleChange<number>('cvv', (value) => parseInt(value, 10))}
+                    required
+                />
+                {errors.cvv && <p className="error">{errors.cvv}</p>}
+                <input
+                    aria-label="Expiry date"
+                    placeholder="Expiry date"
+                    type="Date"
+                    value={cardDetails.expiry || ''}
+                    onChange={handleChange<string>('expiry', (value) => value)}
+                    required
+                />
+                {errors.expiry && <p className="error">{errors.expiry}</p>}
+            </div>
+            <div className="row">
+                <input type="submit" name="submit"></input>
+            </div>
+        </form>
+    );
+}
 
 export default Form;
